fec6487a79dc87d6557c9854c58e8b0df9be6c47
kashnx <kashnii250@gmail.com>
2024-03-29 15:32:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 // var items=["Buy Food","Cook Food","Eat Food"];
 // let workItems=[];
 app.set('views', path.join(__dirname, 'views'));
+app.set('public', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
